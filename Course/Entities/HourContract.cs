caa7195b3e5f076814630e99498cee308a7afe86
RebecaPessoa <rebecahayanny17@gmail.com>
2023-05-01 18:43:41 -0300

    Add project files.

---
+++ Course/Entities/HourContract.cs
@@ -0,0 +1,21 @@
+﻿namespace Course.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract(DateTime date, double vauePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = vauePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
